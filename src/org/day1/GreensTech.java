36ea644d36935a96da0f667b84ede06f6064c7d6
dhanasekardhaya <dhanasekardhaya@gmail.com>
2025-02-04 20:15:43 -0500

    Second Program

---
+++ src/org/day1/GreensTech.java
@@ -0,0 +1,39 @@
+package org.day1;
+
+public class GreensTech {
+
+	private void greensOmr() {
+		System.out.println("Greens OMR");
+	}
+
+	private void greensAdayar() {
+		System.out.println("Greens Adyar");
+
+	}
+
+	private void greensTambaram() {
+		System.out.println("Greens Tambaram");
+
+	}
+
+	private void greensVelacherry() {
+		System.out.println("Greens Velacherry");
+
+	}
+
+	private void greensAnnaNagar() {
+		System.out.println("Greens Anna Nagar");
+
+	}
+
+	public static void main(String[] args) {
+		GreensTech greens = new GreensTech();
+		greens.greensOmr();
+		greens.greensAdayar();
+		greens.greensTambaram();
+		greens.greensVelacherry();
+		greens.greensAnnaNagar();
+
+	}
+
+}
